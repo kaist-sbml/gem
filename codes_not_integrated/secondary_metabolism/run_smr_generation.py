29ef394ff9bec5eb58ca92e9ed6ce7cbdd85e311
root <ehukim@kaist.ac.kr>
2015-05-16 18:32:50 +0900

    Update run_smr_generation.py

--- codes_not_integrated/secondary_metabolism/run_smr_generation.py
+++ codes_not_integrated/secondary_metabolism/run_smr_generation.py
@@ -41,22 +41,17 @@ product = get_product_from_cluster_gbk(inputfile, "genbank")
 cluster_info_dict = get_cluster_info_from_cluster_gbk(inputfile, "genbank")
 
 #locustag_product_monomer_dict: MIGHT NOT BE NECESSARY
-# Extracting information of domains from dictionary file 'cluster_info_dict' and 'dic_t1pks_gene' (VV)
-# dic_t1pks_domain['SAV_942_DM6'] = ['PKS_AT', '(2425-2719)', 'SAV_942']
-# dic_t1pks_gene_domain['SAV_938'] = ['PKS_AT', 'ACP', 'PKS_KS', 'PKS_AT', 'PKS_KR', 'ACP', 'PKS_KS', 'PKS_AT', 'PKS_DH', 'PKS_KR', 'ACP', 'PKS_Docking_Cterm']
+#Check return value
 locustag_domain_dict = get_cluster_domain(cluster_info_dict)
 
-# Extracting information of substrates with their substrates from dictionary file 'cluster_info_dict' and 'dic_t1pks_gene' (VV)
-# dic_t1pks_domain_substrate['SAV_943_M1'] = ['mmal', 'Ethyl_mal', 'pk']
 locustag_monomer_dict = get_cluster_monomers(cluster_info_dict)
 
-# Extracting information of modules from dictionary file 'dic_t1pks_domain' (VV)
-# dic_t1pks_module['SAV_943_M1'] = ['PKS_KS', 'PKS_AT', 'ACP']
-dic_nrps_module = second_metab_module(locustag_domain_dict)
+#check input value
+locustag_module_domain_dict = get_cluster_module(locustag_domain_dict)
 
 # Generating rules for biosynthesis of type I PKS and converting module and its substrate to metabolic reactions
 # For example : dic_converted_metabolic_reaction_without_substrate['SAV_943_M0'] = ['coa': 1, 'nadph': -1, 'nadp': 1, 'hco3': 1, 'h': -1]
-dic_converted_metabolic_reaction_without_substrate = generate_currency_metabolites(dic_nrps_module)
+dic_converted_metabolic_reaction_without_substrate = generate_currency_metabolites(locustag_module_domain_dict)
 
 # generating integrated metabolic reaction without participated substrate such as malonyl-coenzyme A
 #dic_integrated_metabolic_reaction_without_cofactors = integrated_metabolic_reaction1(dic_converted_metabolic_reaction_without_substrate) #####
@@ -73,13 +68,6 @@ dic_converted_metabolic_reaction_without_substrate = generate_currency_metabolit
 # adding product name to the integrated reaction
 # list_of_reaction_set_with_product = {'nadph': -10, 'nadp': 10, 'ahcys': 0, '2mbcoa': -1, 'nad': 0, 'h': -10, 'fadh2': 0, 'malcoa': -7, 'hco3': 13, 'Cluster_05_t1pks_1': 1, 'amet': 0, 'coa': 13, 'h2o': 5, 'nadh': 0, '13dpg': 0, 'mmalcoa': -5, 'pi': 0, 'emalcoa': 0, 'fad': 0}]
 #list_of_reaction_set_with_product = adding_product_to_the_reaction(product, list_of_reaction_set)
-
-# making all metabolic reactions from each putative type 1 PKS gene clusters.
-# dic_reaction_info_set[gene_info] = {reaction_set}
-#     dic_converted_metabolic_reactions_with_gene = making_metabolic_reactions_from_gene_cluster_data(dic_t1pks_gene, list_of_reaction_set_with_product)
-
-# making total list of metabolites in model (e.g. metab_MNXM_dict[MNX_ID(e.g. MNXM37)] = metabolite_name_in_model(e.g. gln_DASH_L_p))
-#     metab_MNXM_dict = COBRA_TemplateModel_checking_MNXref_metabolites(cobra_model, MNXM_dict)
 
 # adding metabolic reactions to the model
 #modified_cobra_model, list_reaction_name_SM, list_novel_secondary_metabolite_reactions = second_metab_reactions_addition(cobra_model, product, locustag_product_monomer_dict, list_of_reaction_set_with_product, metab_MNXM_dict)
