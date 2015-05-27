e723769d95469cf2ecd717118d1e6c5a4eebe192
root <ehukim@kaist.ac.kr>
2015-05-27 20:19:06 +0900

    Integrate secondary metabolic reactions with target_model

--- codes_not_integrated/secondary_metabolism/run_smr_generation.py
+++ codes_not_integrated/secondary_metabolism/run_smr_generation.py
@@ -5,36 +5,48 @@
 
 from cobra.io.sbml import create_cobra_model_from_sbml_file, write_cobra_model_to_sbml_file
 import pickle
+import os
 from sec_met_rxn_generation import get_product_from_cluster_gbk, get_cluster_info_from_cluster_gbk, get_cluster_domain, get_cluster_monomers, get_cluster_module, get_currency_metabolites, get_total_currency_metab_coeff, get_all_metab_coeff, add_sec_met_rxn 
+from sets import Set
 
 print "Generating secondary metabolite biosynthesizing reactions.."
 
 target_model = create_cobra_model_from_sbml_file('sma_target_model_sco.xml')
 #inputfile = './NC_021055.1.cluster002.gbk' #NRPS
 #inputfile = './NC_013929.1.cluster031.gbk' #PKS
-inputfile = './NC_020990.1.cluster023.gbk' #Hybrid
+#inputfile = './NC_020990.1.cluster023.gbk' #Hybrid
 
 bigg_mnxm_compound_dict = pickle.load(open('bigg_mnxm_compound_dict.p','rb'))
 mnxm_compoundInfo_dict = pickle.load(open('mnxm_compoundInfo_dict.p','rb'))
-##########################################################################
 
+#dirname = './target_genome/'
+dirname = './'
+cluster_files = []
+for inputfile in os.listdir(dirname):
+    if inputfile.endswith('.gbk') and 'cluster' in inputfile:
+        cluster_files.append(inputfile)
 
-cluster_info_dict = get_cluster_info_from_cluster_gbk(inputfile, "genbank")
+cluster_files.sort()
 
-product = get_product_from_cluster_gbk(inputfile, "genbank")
+for cluster_f in cluster_files:
+    print cluster_f
 
-locustag_domain_dict = get_cluster_domain(cluster_info_dict)
+    cluster_info_dict = get_cluster_info_from_cluster_gbk(dirname+cluster_f, "genbank")
 
-locustag_monomer_dict = get_cluster_monomers(cluster_info_dict)
+    product = get_product_from_cluster_gbk(dirname+cluster_f, "genbank")
 
-locustag_module_domain_dict = get_cluster_module(locustag_domain_dict)
+    locustag_domain_dict = get_cluster_domain(cluster_info_dict)
 
-module_currency_metab_dict = get_currency_metabolites(locustag_module_domain_dict)
+    locustag_monomer_dict = get_cluster_monomers(cluster_info_dict)
 
-currency_metab_coeff_dict = get_total_currency_metab_coeff(module_currency_metab_dict)
+    locustag_module_domain_dict = get_cluster_module(locustag_domain_dict)
 
-metab_coeff_dict = get_all_metab_coeff(locustag_monomer_dict, currency_metab_coeff_dict, product)
+    module_currency_metab_dict = get_currency_metabolites(locustag_module_domain_dict)
 
-target_model = add_sec_met_rxn(target_model, metab_coeff_dict, product, bigg_mnxm_compound_dict, mnxm_compoundInfo_dict, cluster_info_dict)
+    currency_metab_coeff_dict = get_total_currency_metab_coeff(module_currency_metab_dict)
 
-write_cobra_model_to_sbml_file(target_model, 'sma_target_model_complete.xml')
+    metab_coeff_dict = get_all_metab_coeff(locustag_monomer_dict, currency_metab_coeff_dict, product)
+
+    target_model = add_sec_met_rxn(target_model, metab_coeff_dict, product, bigg_mnxm_compound_dict, mnxm_compoundInfo_dict, cluster_info_dict)
+
+write_cobra_model_to_sbml_file(target_model, 'sma_target_model_sco_complete.xml')
