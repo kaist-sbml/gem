00e6c62daf92b318d0ef4ade23340bf5d0553fb4
Hyun Uk Kim <ehukim@kaist.ac.kr>
2016-06-13 00:17:27 +0900

    run_gbkeditor: remove annotation marks

--- codes_not_integrated/gbk_file_processing/run_gbkeditor.py
+++ codes_not_integrated/gbk_file_processing/run_gbkeditor.py
@@ -9,17 +9,17 @@ import time
 start = time.time()
 
 fileName, outfile, seq_records = read_gbk()
-#get_aa_sequence(outfile, seq_records)
+get_aa_sequence(outfile, seq_records)
 num_cds, num_ec, num_protid, num_trans = count_cds_qualifiers(seq_records)
 get_targetGenomeInfo(seq_records, fileName)
 
 #Retrieving aa sequence often skips some CDSs
-#while num_cds - num_trans > 5:
-#    get_aa_sequence(outfile, seq_records)
-#    num_cds, num_ec, num_protid, num_trans = count_cds_qualifiers(seq_records)
+while num_cds - num_trans > 5:
+    get_aa_sequence(outfile, seq_records)
+    num_cds, num_ec, num_protid, num_trans = count_cds_qualifiers(seq_records)
 
 #Writing an updaged gbk file
-#print "Writing output file with translation"
-#SeqIO.write(seq_records, outfile, "genbank")
+print "Writing output file with translation"
+SeqIO.write(seq_records, outfile, "genbank")
 
 print "elpased time:", time.strftime("%H:%M:%S", time.gmtime(time.time() - start))
