867893f2cf93bcd487885d414502bb90ccd160e7
Hyun Uk Kim <ehukim@kaist.ac.kr>
2017-01-24 23:39:03 +0900

    tests: 'data_dir' -> 'data_model_dir'

--- gems/tests/conftest.py
+++ gems/tests/conftest.py
@@ -8,11 +8,11 @@ from argparse import Namespace
 from cobra.io import read_sbml_model, write_sbml_model
 from os.path import join, abspath, dirname
 
-data_dir = join(dirname(abspath(__file__)), 'data')
+data_model_dir = join(dirname(abspath(__file__)), 'data_model')
 
 @pytest.fixture(scope="function")
 def model():
-    model = read_sbml_model(join(data_dir, 'iMK1208Edited4.xml'))
+    model = read_sbml_model(join(data_model_dir, 'iMK1208Edited4.xml'))
 
     # Returning model is necessary, otherwise error occurs
     return model
@@ -22,7 +22,7 @@ def options():
     options = Namespace()
 
     template_exrxnid_flux_dict = pickle.load(
-             open(join(data_dir, 'sco_tempModel_exrxnid_flux_dict.p'),'rb'))
+             open(join(data_model_dir, 'sco_tempModel_exrxnid_flux_dict.p'),'rb'))
     options.template_exrxnid_flux_dict = template_exrxnid_flux_dict
 
     return options
