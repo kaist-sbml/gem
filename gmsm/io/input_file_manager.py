c289ec656876dff8700e7f2998a464e0f1c5a86a
Mun Su Kwon <kms960621@kaist.ac.kr>
2019-03-25 21:43:06 +0900

    changed input_file_manager

--- gmsm/io/input_file_manager.py
+++ gmsm/io/input_file_manager.py
@@ -18,44 +18,46 @@ def make_folder(folder):
         os.makedirs(folder)
 
 
-def setup_outputfolders(options):
+def setup_outputfolders(io_ns):
+    options = io_ns
     folders = ['1_EFICAz_results', '2_blastp_results',
             '3_primary_metabolic_model', '4_complete_model',
             'tmp_model_files', 'tmp_data_files']
 
     # Second if statement is to keep "-o ./test" from creating "tes", not "test"
-    if '/' in options.outputfolder and './' not in options.outputfolder:
-        options.outputfolder = options.outputfolder[:-1]
+    if '/' in io_ns.outputfolder and './' not in io_ns.outputfolder:
+        io_ns.outputfolder = io_ns.outputfolder[:-1]
 
     if options.eficaz:
         #'1_EFICAz_results'
-        options.outputfolder1 = os.path.join(options.outputfolder, folders[0])
-        make_folder(options.outputfolder1)
+        io_ns.outputfolder1 = os.path.join(options.outputfolder, folders[0])
+        make_folder(io_ns.outputfolder1)
     if options.pmr_generation:
         #'2_blastp_results'
-        options.outputfolder2 = os.path.join(options.outputfolder, folders[1])
-        make_folder(options.outputfolder2)
+        io_ns.outputfolder2 = os.path.join(options.outputfolder, folders[1])
+        make_folder(io_ns.outputfolder2)
         #'3_primary_metabolic_model'
-        options.outputfolder3 = os.path.join(options.outputfolder, folders[2])
-        make_folder(options.outputfolder3)
+        io_ns.outputfolder3 = os.path.join(options.outputfolder, folders[2])
+        make_folder(io_ns.outputfolder3)
     if options.smr_generation:
         #'3_primary_metabolic_model'
-        options.outputfolder3 = os.path.join(options.outputfolder, folders[2])
-        make_folder(options.outputfolder3)
+        io_ns.outputfolder3 = os.path.join(options.outputfolder, folders[2])
+        make_folder(io_ns.outputfolder3)
         #'4_complete_model'
-        options.outputfolder4 = os.path.join(options.outputfolder, folders[3])
-        make_folder(options.outputfolder4)
+        io_ns.outputfolder4 = os.path.join(options.outputfolder, folders[3])
+        make_folder(io_ns.outputfolder4)
 
     #'tmp_model_files'
-    options.outputfolder5 = os.path.join(options.outputfolder, folders[4])
-    make_folder(options.outputfolder5)
+    io_ns.outputfolder5 = os.path.join(options.outputfolder, folders[4])
+    make_folder(io_ns.outputfolder5)
 
     #'tmp_data_files'
-    options.outputfolder6 = os.path.join(options.outputfolder, folders[5])
-    make_folder(options.outputfolder6)
+    io_ns.outputfolder6 = os.path.join(options.outputfolder, folders[5])
+    make_folder(io_ns.outputfolder6)
 
 
-def show_input_options(options):
+def show_input_options(io_ns):
+    options = io_ns
     logging.debug("input_file: %s", options.input)
     logging.debug("outputfolder: %s", options.outputfolder)
     logging.debug("template_model_organism: %s", options.orgName)
@@ -66,7 +68,8 @@ def show_input_options(options):
     logging.debug("compartment_file: %s", options.comp)
 
 
-def check_input_filetype(options):
+def check_input_filetype(io_ns):
+    options = io_ns
     input_ext = os.path.splitext(options.input)[1]
 
     if input_ext in ('.gbk', '.gb', '.genbank', '.gbf', '.gbff'):
@@ -77,12 +80,12 @@ def check_input_filetype(options):
         return 'fasta'
 
 
-def get_target_genome_from_input(filetype, options):
-
-    options.targetGenome_locusTag_aaSeq_dict = {}
-    options.targetGenome_locusTag_ec_dict = {}
-    options.targetGenome_locusTag_prod_dict = {}
-    options.total_cluster = 0
+def get_target_genome_from_input(filetype, io_ns):
+    options = io_ns
+    io_ns.targetGenome_locusTag_aaSeq_dict = {}
+    io_ns.targetGenome_locusTag_ec_dict = {}
+    io_ns.targetGenome_locusTag_prod_dict = {}
+    io_ns.total_cluster = 0
 
     seq_records = list(SeqIO.parse(options.input, filetype))
 
@@ -96,11 +99,11 @@ def get_target_genome_from_input(filetype, options):
 
         if filetype == 'genbank':
             for seq_record in seq_records:
-                get_features_from_gbk(seq_record, options)
+                get_features_from_gbk(seq_record, io_ns)
 
         elif filetype == 'fasta':
             for seq_record in seq_records:
-                get_features_from_fasta(seq_record, options)
+                get_features_from_fasta(seq_record, io_ns)
 
 
     #Number of 'locus_tag's obtained above may be different from
@@ -109,18 +112,18 @@ def get_target_genome_from_input(filetype, options):
     #which are not considered herein.
     #Same above comment for 'product's
     logging.debug(
-                "len(options.targetGenome_locusTag_prod_dict.keys):%s"
-                %len(options.targetGenome_locusTag_prod_dict.keys()))
+                "len(io_ns.targetGenome_locusTag_prod_dict.keys):%s"
+                %len(io_ns.targetGenome_locusTag_prod_dict.keys()))
 
     logging.debug(
-                "len(options.targetGenome_locusTag_ec_dict.keys): %s"
-                %len(options.targetGenome_locusTag_ec_dict.keys()))
+                "len(io_ns.targetGenome_locusTag_ec_dict.keys): %s"
+                %len(io_ns.targetGenome_locusTag_ec_dict.keys()))
 
     return seq_records
 
 
-def get_eficaz_file(options):
-
+def get_eficaz_file(io_ns):
+    options = io_ns
     logging.info("Reading EFICAz output file..")
 
 #    EC4Info = {}
@@ -147,10 +150,10 @@ def get_eficaz_file(options):
         #    if r:
         #        EC = r.group(1) + ".-"
         #        #ECDesc = r.group(2)
-        #        if not options.targetGenome_locusTag_ec_dict.has_key(locustag):
-        #            options.targetGenome_locusTag_ec_dict[locustag] = []
+        #        if not io_ns.targetGenome_locusTag_ec_dict.has_key(locustag):
+        #            io_ns.targetGenome_locusTag_ec_dict[locustag] = []
         #            #EC3Info[locustag] = []
-        #        options.targetGenome_locusTag_ec_dict[locustag].append(EC)
+        #        io_ns.targetGenome_locusTag_ec_dict[locustag].append(EC)
         #        #EC3Info[locustag].append(ECDesc)
         #        continue
 
@@ -164,10 +167,10 @@ def get_eficaz_file(options):
             if r:
                 EC = r.group(1)
                 #ECDesc = r.group(2)
-                if not options.targetGenome_locusTag_ec_dict.has_key(locustag):
-                    options.targetGenome_locusTag_ec_dict[locustag] = []
+                if not io_ns.targetGenome_locusTag_ec_dict.has_key(locustag):
+                    io_ns.targetGenome_locusTag_ec_dict[locustag] = []
                     #EC4Info[locustag] = []
-                options.targetGenome_locusTag_ec_dict[locustag].append(EC)
+                io_ns.targetGenome_locusTag_ec_dict[locustag].append(EC)
                 #EC4Info[locustag].append(ECDesc)
                 continue
 
@@ -175,56 +178,56 @@ def get_eficaz_file(options):
                         "Locus tag: %s; EC number with 4 digits: %s", locustag, EC)
     f.close()
 
-    logging.debug("len(options.targetGenome_locusTag_ec_dict.keys): %s",
-                  len(options.targetGenome_locusTag_ec_dict.keys()))
+    logging.debug("len(io_ns.targetGenome_locusTag_ec_dict.keys): %s",
+                  len(io_ns.targetGenome_locusTag_ec_dict.keys()))
 
 
-def get_fasta_files(options):
+def get_fasta_files(io_ns):
     #Following data are needed only for primary metabolic modeling
     logging.info("Looking for a fasta file of a target genome..")
-    get_target_fasta(options)
+    get_target_fasta(io_ns)
 
     logging.info("Looking for a fasta file of template model genes..")
-    get_temp_fasta(options)
+    get_temp_fasta(io_ns)
 
 
 #For model pruning phase
 #Only model file is not saved in Namespace
-def get_pickles_prunPhase(options):
+def get_pickles_prunPhase(io_ns):
     logging.info("Loading pickle files associated with a template model..")
-    model = pickle.load(open('%s/model.p' %(options.input1),'rb'))
+    model = pickle.load(open('%s/model.p' %(io_ns.input1),'rb'))
     tempModel_biggRxnid_locusTag_dict = pickle.load(open(
-        '%s/tempModel_biggRxnid_locusTag_dict.p' %(options.input1),'rb'))
-    options.tempModel_biggRxnid_locusTag_dict = tempModel_biggRxnid_locusTag_dict
+        '%s/tempModel_biggRxnid_locusTag_dict.p' %(io_ns.input1),'rb'))
+    io_ns.tempModel_biggRxnid_locusTag_dict = tempModel_biggRxnid_locusTag_dict
 
     return model
 
 
 #For model augmentation phase in both primary and secondary modeling
-def get_pickles_augPhase(options):
+def get_pickles_augPhase(io_ns):
     logging.info("Loading pickle files necessary for the model augmentation phase..")
 
     bigg_mnxr_dict = pickle.load(open('./gmsm/io/data/input2/bigg_mnxr_dict.p','rb'))
-    options.bigg_mnxr_dict = bigg_mnxr_dict
+    io_ns.bigg_mnxr_dict = bigg_mnxr_dict
 
     mnxm_compoundInfo_dict = pickle.load(open('./gmsm/io/data/input2/mnxm_compoundInfo_dict.p','rb'))
-    options.mnxm_compoundInfo_dict = mnxm_compoundInfo_dict
+    io_ns.mnxm_compoundInfo_dict = mnxm_compoundInfo_dict
 
     mnxr_kegg_dict = pickle.load(open('./gmsm/io/data/input2/mnxr_kegg_dict.p','rb'))
-    options.mnxr_kegg_dict = mnxr_kegg_dict
+    io_ns.mnxr_kegg_dict = mnxr_kegg_dict
 
     mnxref = pickle.load(open('./gmsm/io/data/input2/MNXref.p','rb'))
-    options.mnxref = mnxref
+    io_ns.mnxref = mnxref
 
-    template_exrxnid_flux_dict = pickle.load(open('%s/tempModel_exrxnid_flux_dict.p' %(options.input1),'rb'))
-    options.template_exrxnid_flux_dict = template_exrxnid_flux_dict
+    template_exrxnid_flux_dict = pickle.load(open('%s/tempModel_exrxnid_flux_dict.p' %(io_ns.input1),'rb'))
+    io_ns.template_exrxnid_flux_dict = template_exrxnid_flux_dict
 
 
-def get_locustag_comp_dict(options):
-
+def get_locustag_comp_dict(io_ns):
+    options = io_ns
     logging.info("Reading file on subcellular localizations (compartments)..")
 
-    options.locustag_comp_dict = {}
+    io_ns.locustag_comp_dict = {}
 
     try:
         f = open(options.comp,"r")
@@ -244,13 +247,13 @@ def get_locustag_comp_dict(options):
         comp = comp.strip()
         logging.debug("Locus tag: %s; Predicted compartment: %s", locustag, comp)
 
-        if locustag not in options.locustag_comp_dict:
-            options.locustag_comp_dict[locustag] = [comp]
+        if locustag not in io_ns.locustag_comp_dict:
+            io_ns.locustag_comp_dict[locustag] = [comp]
         else:
-            options.locustag_comp_dict[locustag].append(comp)
+            io_ns.locustag_comp_dict[locustag].append(comp)
 
     f.close()
 
-    logging.debug("len(options.locustag_comp_dict.keys): %s",
-                  len(options.locustag_comp_dict.keys()))
+    logging.debug("len(io_ns.locustag_comp_dict.keys): %s",
+                  len(io_ns.locustag_comp_dict.keys()))
 
