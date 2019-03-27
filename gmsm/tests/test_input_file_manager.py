8b28616999f5207c92e3aef61836330649fd5bbd
Mun Su Kwon <kms960621@kaist.ac.kr>
2019-03-27 14:09:40 +0900

    test_input_file_manager.py : add arguments in get_eficaz_file and get_locustag_comp_dict

--- gmsm/tests/test_input_file_manager.py
+++ gmsm/tests/test_input_file_manager.py
@@ -10,7 +10,7 @@ class TestInput_file_manager:
 
         options.eficaz_file = eficaz_file
         options.targetGenome_locusTag_ec_dict = {}
-        get_eficaz_file(options)
+        get_eficaz_file(options, options)
 
         assert len(options.targetGenome_locusTag_ec_dict) == 2
         assert options.targetGenome_locusTag_ec_dict['NSK_00005-RA'] == ['2.7.1.83']
@@ -19,7 +19,7 @@ class TestInput_file_manager:
     def test_get_locustag_comp_dict(self, comp_file, options):
 
         options.comp = comp_file
-        get_locustag_comp_dict(options)
+        get_locustag_comp_dict(options, options)
 
         assert len(options.locustag_comp_dict) == 8
         assert options.locustag_comp_dict['NSK_00004-RA'] == ['h']
